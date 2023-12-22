35e731006484282ed11a50c984c874907c66b7ec
GTerLeon <Leon.Ter.Etu@univ-lemans.fr>
2023-12-22 10:43:37 +0100

    update petitionendpoint

--- src/main/java/foo/PetitionEndpoint.java
+++ src/main/java/foo/PetitionEndpoint.java
@@ -1,111 +1,95 @@
 package foo;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
-
-import com.google.api.server.spi.auth.common.User;
+import java.util.ArrayList;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
-import com.google.api.server.spi.config.ApiMethod.HttpMethod;
-import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.config.Named;
 import com.google.api.server.spi.config.Nullable;
-import com.google.api.server.spi.response.CollectionResponse;
+import com.google.api.server.spi.config.ApiNamespace;
 import com.google.api.server.spi.response.UnauthorizedException;
-import com.google.api.server.spi.auth.EspAuthenticator;
-
-import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.PropertyProjection;
-import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
-import com.google.appengine.api.datastore.Query.CompositeFilter;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterOperator;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.Query.SortDirection;
-import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Transaction;
-import com.google.appengine.api.datastore.KeyFactory;
+import com.google.api.server.spi.auth.common.User;
 
-@Api(name = "myApi",
-     version = "v1",
-     namespace = @ApiNamespace(ownerDomain = "helloworld.example.com",
-                               ownerName = "helloworld.example.com",
-                               packagePath = "")
+@Api(
+    name = "myApi",
+    version = "v1",
+    namespace = @ApiNamespace(
+        ownerDomain = "tinypet.example.com",
+        ownerName = "tinypet.example.com",
+        packagePath = "")
 )
-
 public class PetitionEndpoint {
 
-    @ApiMethod(name = "createPetition", path = "createPetition", httpMethod = HttpMethod.POST)
-    public Entity createPetition(@Named("title") String title,
-                                 @Named("description") String description,
-                                 @Named("tags") String tags) {
+    private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    @ApiMethod(name = "createPetition", httpMethod = ApiMethod.HttpMethod.POST)
+    public Entity createPetition(User user, @Named("title") String title, @Named("description") String description, @Named("tags") String tags) throws UnauthorizedException {
+        if (user == null) {
+            throw new UnauthorizedException("User must be signed in to create a petition.");
+        }
         Entity petitionEntity = new Entity("Petition");
+        petitionEntity.setProperty("owner", user.getEmail());
         petitionEntity.setProperty("title", title);
         petitionEntity.setProperty("description", description);
         petitionEntity.setProperty("tags", tags);
-        petitionEntity.setProperty("signatureCount", 0); // Start with zero signatures
-        petitionEntity.setProperty("createdDate", new Date()); // Set the current date as the creation date
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(petitionEntity); // Save the entity to the Datastore
-
+        petitionEntity.setProperty("signatureCount", 0);
+        petitionEntity.setProperty("createdDate", new Date());
+        datastore.put(petitionEntity);
         return petitionEntity;
     }
 
-    @ApiMethod(name = "userSignedPetitions", path = "userSignedPetitions/{userId}", httpMethod = HttpMethod.GET)
-    public List<Entity> userSignedPetitions(@Named("userId") String userId) {
-        // Create a query for petitions signed by the user
-        Query q = new Query("Petition").setFilter(new FilterPredicate("signatures", FilterOperator.EQUAL, userId));
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        PreparedQuery pq = datastore.prepare(q);
-        List<Entity> result = pq.asList(FetchOptions.Builder.withLimit(100));
-
-        return result;
-    }
-
-    @ApiMethod(name = "signPetition", path = "signPetition", httpMethod = HttpMethod.POST)
-    public void signPetition(@Named("petitionId") Long petitionId, @Named("userId") String userId) {
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    @ApiMethod(name = "signPetition", httpMethod = ApiMethod.HttpMethod.POST)
+    public void signPetition(User user, @Named("petitionId") long petitionId) throws UnauthorizedException, EntityNotFoundException {
+        if (user == null) {
+            throw new UnauthorizedException("User must be signed in to sign a petition.");
+        }
         Transaction txn = datastore.beginTransaction();
         try {
-            // Get the Petition entity from the Datastore
-            Entity petitionEntity = datastore.get(KeyFactory.createKey("Petition", petitionId));
-            
-            // Update the signatures property with the user's ID
+            Key key = KeyFactory.createKey("Petition", petitionId);
+            Entity petitionEntity = datastore.get(key);
             List<String> signatures = (List<String>) petitionEntity.getProperty("signatures");
             if (signatures == null) {
                 signatures = new ArrayList<>();
             }
-            if (!signatures.contains(userId)) {
-                signatures.add(userId);
+            if (!signatures.contains(user.getEmail())) {
+                signatures.add(user.getEmail());
                 petitionEntity.setProperty("signatures", signatures);
-                
-                // Save the petition entity back to the Datastore
-                datastore.put(txn, petitionEntity);
-                
-                // Commit the transaction
+                petitionEntity.setProperty("signatureCount", signatures.size());
+                datastore.put(petitionEntity);
                 txn.commit();
             }
-        } catch (Exception e) {
-            // If there are any errors, print the stack trace for debugging
-            e.printStackTrace();
         } finally {
             if (txn.isActive()) {
-                txn.rollback(); // Rollback in case of errors or if the transaction is still active
+                txn.rollback();
             }
         }
-}
+    }
+
+    @ApiMethod(name = "getMySignedPetitions", httpMethod = ApiMethod.HttpMethod.GET)
+    public List<Entity> getMySignedPetitions(User user) throws UnauthorizedException {
+        if (user == null) {
+            throw new UnauthorizedException("User must be signed in.");
+        }
+        Query query = new Query("Petition").setFilter(new Query.FilterPredicate("signatures", Query.FilterOperator.EQUAL, user.getEmail()));
+        List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withDefaults());
+        return results;
+    }
+
+    @ApiMethod(name = "getTopPetitions", httpMethod = ApiMethod.HttpMethod.GET)
+    public List<Entity> getTopPetitions() {
+        Query query = new Query("Petition").addSort("signatureCount", Query.SortDirection.DESCENDING);
+        List<Entity> results = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(100));
+        return results;
+    }
 }
